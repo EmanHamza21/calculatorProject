4fab865882fe247e6036e0ac0e5512487e045fbd
Eman Hamza <ehamza@kwentra.com>
2022-09-23 15:42:56 +0200

    assignment-1 solution for the assignment

---
+++ assignment.js
@@ -0,0 +1,11 @@
+let userInput = 0;
+let result;
+
+result = 18 + `${userInput}`;
+result = result - 2 * 2 / 2;
+console.log(result);
+console.log(userInput);
+
+
+alert('This is the results', result);
+alert('This is the user input', userInput);
